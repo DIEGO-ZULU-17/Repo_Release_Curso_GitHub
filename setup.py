2203ad3f8a96497c43ce5e85437030013b2024d1
Diego.Zuluaga <diegozulu17@outlook.com>
2026-05-02 01:57:08 -0500

    Lanzamiento del primer paquete

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Repo_Release_Curso_GitHub",               # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Diego Zuluaga",                         # Tu nombre
+    author_email="diegozulu17@gmail.com",                                     # Tu correo electrónico
+    url="https://github.com/DIEGO-ZULU-17/Repo_Release_Curso_GitHub.git",     # URL del proyecto
+)
